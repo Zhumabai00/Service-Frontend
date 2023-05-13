7025be7d7af25d1244da3df783a74faf130b4fbb
user <user@MacBook-Pro.local>
2023-05-14 04:04:36 +0600

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faBell } from '@fortawesome/free-solid-svg-icons';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
-import './index.css';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
+      {/* <FontAwesomeIcon icon={faEnvelope} /> */}
       <Header />
       <Main />
       <Footer />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,36 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faBell } from '@fortawesome/free-solid-svg-icons';
+import userImg from '../../img/icons/regularUsers.png'
 import './Header.scss'
 
 function Header() {
 	return (
-		<div className="Header">
-
+		<div className="header">
+			<div className="container">
+				<div className="header__body">
+					<div className="header__menu">
+						<div className="header__logo"><h1><a href="">Servise</a></h1></div>
+						<div className="header__menu-list">
+							<ul className="header__list">
+								<li className="header__link"><a href="#">Создать задание</a></li>
+								<li className="header__link"><a href="#">Найти задания</a></li>
+								<li className="header__link"><a href="#">Мои заказы</a></li>
+							</ul>
+						</div>
+					</div>
+					<div className="header__user user">
+						<div className="user__message">
+							<a href="">
+								<FontAwesomeIcon icon={faBell} />
+							</a>
+						</div>
+						<div className="user__icon">
+							<img src={userImg} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
